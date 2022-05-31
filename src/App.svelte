73cfb20e2e16ba41df4db6989e5bb55487bc196e
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-01 09:06:07 +1000

    Indenting Constants

--- src/App.svelte
+++ src/App.svelte
@@ -6,30 +6,31 @@
 		anionNames: ["fluoride", "chloride", "bromide", "iodide", "hydroxide", "thiocyanate", "nitrate", "acetate", "carbonate", "sulfate", "phosphate"],
 		cationProperNames: ["ammonium", "hydrogen", "lithium", "sodium", "potassium", "beryllium", "magnesium", "calcium", "strontium", "barium", "aluminium", "manganese(II)", "iron(II)", "cobalt(II)", "nickel(II)", "copper(II)", "zinc(II)", "mercury(II)", "lead(II)", "chromium(III)", "iron(III)", "silver"],
 		cationCommonNames: ["ammonium", "hydrogen", "lithium", "sodium", "potassium", "beryllium", "magnesium", "calcium", "strontium", "barium", "aluminium", "manganese", "ferrous", "cobaltous", "nickelous", "cupric", "zinc", "mercuric", "plumbous", "chromic", "ferric", "silver"],
-	reactionTable: [
-	[["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-	[["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","Green"],["S","GhostWhite"],["S","GhostWhite"]],
-	[["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-	[["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-	[["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-	[["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"]],
-	[["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["I","Ivory"]],
-	[["I","SkyBlue"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
-	[["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","SkyBlue"],["I","Ivory"]],
-	[["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
-	[["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Khaki"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["R","Al(OH)3","I","Ivory","aluminium hydroxide"],["S","GhostWhite"],["I","Ivory"]],
-	[["S","Pink"],["S","Pink"],["S","Pink"],["S","Pink"],["I","Peru"],["I","Ivory"],["S","GhostWhite"],["S","Pink"],["I","Pink"],["S","Pink"],["I","Pink"]],
-	[["S","GhostWhite"],["S","Green"],["S","Khaki"],["S","GhostWhite"],["I","YellowGreen"],["S","GhostWhite"],["S","Green"],["S","YellowGreen"],["I","YellowGreen"],["S","YellowGreen"],["I","SandyBrown"]],
-	[["S","Salmon"],["S","Salmon"],["S","Violet"],["S","Green"],["I","Blue"],["S","Brown"],["S","Red"],["S","Pink"],["I","Purple"],["S","Red"],["I","Violet"]],
-	[["S","Green"],["S","YellowGreen"],["S","Blue"],["S","Green"],["I","YellowGreen"],["S","GoldenRod"],["S","Green"],["S","Cyan"],["I","YellowGreen"],["S","Blue"],["I","YellowGreen"]],
-	[["I","Cyan"],["S","Blue"],["S","DarkGreen"],["R","CuI","I","Ivory", "copper(I) iodide"],["I","Blue"],["I","Black"],["S","Blue"],["S","DarkBlue"],["R","Cu2(OH)2CO3","I","SeaGreen", "copper(II) carbonate hydroxide"],["S","Blue"],["I","SkyBlue"]],
-	[["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["I","Ivory"]],
-	[["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","OrangeRed"],["R","HgO","I","Orange", "mercury(II) oxide"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Yellow"],["I","Ivory"]],
-	[["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Yellow"],["I","Ivory"],["I","Khaki"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
-	[["I","DarkGreen"],["S","DarkGreen"],["S","DarkGreen"],["S","DarkGreen"],["I","Green"],["S","Green"],["S","DarkBlue"],["S","DarkGreen"],["I","LightCyan"],["I","CadetBlue"],["I","DarkOrchid"]],
-	[["S","AntiqueWhite"],["S","Yellow"],["S","DarkBrown"],["R","FeI2","S","GhostWhite", "iron(II) iodide"],["I","DarkOrange"],["S","Red"],["S","Red"],["S","IndianRed"],["R","Fe2O3","I","Red", "iron(III) oxide"],["S","Orange"],["I","Peru"]],
-	[["S","GhostWhite"],["I","Ivory"],["I","Khaki"],["I","Yellow"],["I","Peru"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Yellow"],["I","Ivory"],["I","Peru"]]
-	]
+    
+    reactionTable: [
+      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
+      [["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","Green"],["S","GhostWhite"],["S","GhostWhite"]],
+      [["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
+      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
+      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
+      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"]],
+      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["I","Ivory"]],
+      [["I","SkyBlue"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
+      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","SkyBlue"],["I","Ivory"]],
+      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
+      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Khaki"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["R","Al(OH)3","I","Ivory","aluminium hydroxide"],["S","GhostWhite"],["I","Ivory"]],
+      [["S","Pink"],["S","Pink"],["S","Pink"],["S","Pink"],["I","Peru"],["I","Ivory"],["S","GhostWhite"],["S","Pink"],["I","Pink"],["S","Pink"],["I","Pink"]],
+      [["S","GhostWhite"],["S","Green"],["S","Khaki"],["S","GhostWhite"],["I","YellowGreen"],["S","GhostWhite"],["S","Green"],["S","YellowGreen"],["I","YellowGreen"],["S","YellowGreen"],["I","SandyBrown"]],
+      [["S","Salmon"],["S","Salmon"],["S","Violet"],["S","Green"],["I","Blue"],["S","Brown"],["S","Red"],["S","Pink"],["I","Purple"],["S","Red"],["I","Violet"]],
+      [["S","Green"],["S","YellowGreen"],["S","Blue"],["S","Green"],["I","YellowGreen"],["S","GoldenRod"],["S","Green"],["S","Cyan"],["I","YellowGreen"],["S","Blue"],["I","YellowGreen"]],
+      [["I","Cyan"],["S","Blue"],["S","DarkGreen"],["R","CuI","I","Ivory", "copper(I) iodide"],["I","Blue"],["I","Black"],["S","Blue"],["S","DarkBlue"],["R","Cu2(OH)2CO3","I","SeaGreen", "copper(II) carbonate hydroxide"],["S","Blue"],["I","SkyBlue"]],
+      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["I","Ivory"]],
+      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","OrangeRed"],["R","HgO","I","Orange", "mercury(II) oxide"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Yellow"],["I","Ivory"]],
+      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Yellow"],["I","Ivory"],["I","Khaki"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
+      [["I","DarkGreen"],["S","DarkGreen"],["S","DarkGreen"],["S","DarkGreen"],["I","Green"],["S","Green"],["S","DarkBlue"],["S","DarkGreen"],["I","LightCyan"],["I","CadetBlue"],["I","DarkOrchid"]],
+      [["S","AntiqueWhite"],["S","Yellow"],["S","DarkBrown"],["R","FeI2","S","GhostWhite", "iron(II) iodide"],["I","DarkOrange"],["S","Red"],["S","Red"],["S","IndianRed"],["R","Fe2O3","I","Red", "iron(III) oxide"],["S","Orange"],["I","Peru"]],
+      [["S","GhostWhite"],["I","Ivory"],["I","Khaki"],["I","Yellow"],["I","Peru"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Yellow"],["I","Ivory"],["I","Peru"]]
+    ]
 	
 	}
 

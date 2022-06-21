07cea75c85bad8879cb31b09020dac08fb63ca71
Felix Montanari <captainboggle108@gmail.com>
2022-06-21 10:48:13 +1000

    did lots

--- src/App.svelte
+++ src/App.svelte
@@ -33,20 +33,66 @@
 	};
 
 const Beaker = {
-  solids: [],
-  solutes: [],
+  solids: [], // solids in the beaker
+  solutes: [], // solutes in the beaker
+  fillLevel: 0, // fill level of the beaker
 
-  addSalts() { // add any number of salts to beaker
+  addSalts() { // add one or two salts to beaker
     for (let i = 0; i < arguments.length; i++) {
       arguments[i].soluble ? this.solids.push(arguments[i]) : this.solutes.push(arguments[i]);
     }
   },
 
   reactSalts() { // react all salts in beaker
-    return true;
+    if (this.solutes.length != 2) {
+      return;
+    }
+
+    let tempSalts = ["",""];
+    
+    tempSalts[0] = reactIons([saltFormulaToIons(this.solutes[0].formula)[0],saltFormulaToIons(this.solutes[1].formula)[1]])
+    tempSalts[1] = reactIons([saltFormulaToIons(this.solutes[1].formula)[0],saltFormulaToIons(this.solutes[0].formula)[1]])
+    
+    if (tempSalts[0].soluble == false) {
+      this.addSalts(tempSalts[0]); // add the new salt to the beaker
+      if (tempSalts[1].soluble == false) {
+        this.addSalts(tempSalts[1]); // if both salts react to each other, add both salts to beaker
+        this.solutes = []; // clear solutes 
+      }
+      else {
+        this.solutes = [tempSalts[1]]; // if only one salt reacts to each other, add the new salt to beaker and clear solutes
+      }        
+    }
+    
+    else if (tempSalts[1].soluble == false) {
+        this.addSalts(tempSalts[1]); // add the new salt to the beaker
+        this.solutes = [tempSalts[0]]; // if only one salt reacts to each other, add the new salt to beaker and clear solutes
+    }
+
+    return;
+  },
+
+  
+
+  getVisualState() { // get the visual state of the beaker
+    let state = {fillLevel: this.fillLevel, solidsColourA: "", solidsColourB: "", solutesColour: "snow", solidsVisible: 0};
+
+    if (this.solids.length != 0) {
+      state.solidsVisible = 1;
+      state.solidsColourA = this.solids[0].colour;
+      state.solidsColourB = this.solids[this.solids.length() - 1].colour;
+    }
+   
+    if (this.solutes.length != 0) {
+
+      return;
+    }
+
+    return state;
   }
 
-}
+  }
+
 
   function getHCF(x, y) {
     // find highest common factor of two numbers
@@ -209,7 +255,51 @@ const Beaker = {
     return salt;
   }
 
- 
+
+  function dataColourToHex(dataTable)
+{
+    var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+    "honeydew":"#f0fff0","hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead","navy":"#000080",
+    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+    "yellow":"#ffff00","yellowgreen":"#9acd32"};
+
+    for (let i = 0; i < dataTable.length; i++) // for each row
+    {
+        for (let j = 0; j<dataTable[i].length; j++) // for each column
+        {
+          if (dataTable[i][j][0] == "I" || data[i][j][0] == "S") // if the entry starts with I or S, the colour will be at index 1
+            {
+              dataTable[i][j][1] = colours[dataTable[i][j][1].toLowerCase()]; // convert
+            }
+            else { // otherwise it will be at index 3
+              dataTable[i][j][3]=colours[dataTable[i][j][3].toLowerCase()]; // convert
+            }
+        }
+    }
+    
+    return dataTable;
+}
+  console.log(dataColourToHex(Constants.reactionTable));
   let selectedCation = "Ca++";
   let selectedAnion = "Cl-";
 </script>

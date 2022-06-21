65e4f15001511a541ec3eb058265c2b3bdd530f9
Felix Montanari <captainboggle108@gmail.com>
2022-06-21 11:40:03 +1000

    converted table to hex, removed temporary conversion function

--- src/App.svelte
+++ src/App.svelte
@@ -7,28 +7,28 @@
 		cationProperNames: ["ammonium", "hydrogen", "lithium", "sodium", "potassium", "beryllium", "magnesium", "calcium", "strontium", "barium", "aluminium", "manganese(II)", "iron(II)", "cobalt(II)", "nickel(II)", "copper(II)", "zinc(II)", "mercury(II)", "lead(II)", "chromium(III)", "iron(III)", "silver"],
 		cationCommonNames: ["ammonium", "hydrogen", "lithium", "sodium", "potassium", "beryllium", "magnesium", "calcium", "strontium", "barium", "aluminium", "manganese", "ferrous", "cobaltous", "nickelous", "cupric", "zinc", "mercuric", "plumbous", "chromic", "ferric", "silver"],
     reactionTable: [
-      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-      [["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"]],
-      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"]],
-      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["I","Ivory"]],
-      [["I","SkyBlue"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
-      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","SkyBlue"],["I","Ivory"]],
-      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
-      [["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Khaki"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["R","Al(OH)3","I","Ivory","aluminium hydroxide"],["S","GhostWhite"],["I","Ivory"]],
-      [["S","Pink"],["S","Pink"],["S","Pink"],["S","Pink"],["I","Peru"],["I","Ivory"],["S","GhostWhite"],["S","Pink"],["I","Pink"],["S","Pink"],["I","Pink"]],
-      [["S","GhostWhite"],["S","Green"],["S","Khaki"],["S","GhostWhite"],["I","YellowGreen"],["S","GhostWhite"],["S","Green"],["S","YellowGreen"],["I","YellowGreen"],["S","YellowGreen"],["I","SandyBrown"]],
-      [["S","Salmon"],["S","Salmon"],["S","Violet"],["S","Green"],["I","Blue"],["S","Brown"],["S","Red"],["S","Pink"],["I","Purple"],["S","Red"],["I","Violet"]],
-      [["S","Green"],["S","YellowGreen"],["S","Blue"],["S","Green"],["I","YellowGreen"],["S","GoldenRod"],["S","Green"],["S","Cyan"],["I","YellowGreen"],["S","Blue"],["I","YellowGreen"]],
-      [["I","Cyan"],["S","Blue"],["S","DarkGreen"],["R","CuI","I","Ivory", "copper(I) iodide"],["I","Blue"],["I","Black"],["S","Blue"],["S","DarkBlue"],["R","Cu2(OH)2CO3","I","SeaGreen", "copper(II) carbonate hydroxide"],["S","Blue"],["I","SkyBlue"]],
-      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["S","GhostWhite"],["I","Ivory"]],
-      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","OrangeRed"],["R","HgO","I","Orange", "mercury(II) oxide"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Yellow"],["I","Ivory"]],
-      [["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Yellow"],["I","Ivory"],["I","Khaki"],["S","GhostWhite"],["S","GhostWhite"],["I","Ivory"],["I","Ivory"],["I","Ivory"]],
-      [["I","DarkGreen"],["S","DarkGreen"],["S","DarkGreen"],["S","DarkGreen"],["I","Green"],["S","Green"],["S","DarkBlue"],["S","DarkGreen"],["I","LightCyan"],["I","CadetBlue"],["I","DarkOrchid"]],
-      [["S","AntiqueWhite"],["S","Yellow"],["S","saddlebrown"],["R","FeI2","S","GhostWhite", "iron(II) iodide"],["I","DarkOrange"],["S","Red"],["S","Red"],["S","IndianRed"],["R","Fe2O3","I","Red", "iron(III) oxide"],["S","Orange"],["I","Peru"]],
-      [["S","GhostWhite"],["I","Ivory"],["I","Khaki"],["I","Yellow"],["I","Peru"],["I","Ivory"],["S","GhostWhite"],["S","GhostWhite"],["I","Yellow"],["I","Ivory"],["I","Peru"]],
-      [["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","GhostWhite"],["S","Yellow"],["S","GhostWhite"],["S","Green"],["S","GhostWhite"],["S","GhostWhite"]]
+      [["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#ffff00"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"]],
+      [["S","#f8f8ff"],["S","#ffff00"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"]],
+      [["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"]],
+      [["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"]],
+      [["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"]],
+      [["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["S","#f8f8ff"],["I","#fffff0"]],
+      [["I","#87ceeb"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["I","#fffff0"],["I","#fffff0"]],
+      [["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["I","#87ceeb"],["I","#fffff0"]],
+      [["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["I","#fffff0"],["I","#fffff0"]],
+      [["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f0e68c"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["R","Al(OH)3","I","#fffff0","aluminium hydroxide"],["S","#f8f8ff"],["I","#fffff0"]],
+      [["S","#ffc0cb"],["S","#ffc0cb"],["S","#ffc0cb"],["S","#ffc0cb"],["I","#cd853f"],["I","#fffff0"],["S","#f8f8ff"],["S","#ffc0cb"],["I","#ffc0cb"],["S","#ffc0cb"],["I","#ffc0cb"]],
+      [["S","#f8f8ff"],["S","#008000"],["S","#f0e68c"],["S","#f8f8ff"],["I","#9acd32"],["S","#f8f8ff"],["S","#008000"],["S","#9acd32"],["I","#9acd32"],["S","#9acd32"],["I","#f4a460"]],
+      [["S","#fa8072"],["S","#fa8072"],["S","#ee82ee"],["S","#008000"],["I","#0000ff"],["S","#a52a2a"],["S","#ff0000"],["S","#ffc0cb"],["I","#800080"],["S","#ff0000"],["I","#ee82ee"]],
+      [["S","#008000"],["S","#9acd32"],["S","#0000ff"],["S","#008000"],["I","#9acd32"],["S","#daa520"],["S","#008000"],["S","#00ffff"],["I","#9acd32"],["S","#0000ff"],["I","#9acd32"]],
+      [["I","#00ffff"],["S","#0000ff"],["S","#006400"],["R","CuI","I","#fffff0","copper(I) iodide"],["I","#0000ff"],["I","#000000"],["S","#0000ff"],["S","#00008b"],["R","Cu2(OH)2CO3","I","#2e8b57","copper(II) carbonate hydroxide"],["S","#0000ff"],["I","#87ceeb"]],
+      [["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["S","#f8f8ff"],["I","#fffff0"]],
+      [["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#ff4500"],["R","HgO","I","#ffa500","mercury(II) oxide"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["I","#ffff00"],["I","#fffff0"]],
+      [["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#ffff00"],["I","#fffff0"],["I","#f0e68c"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#fffff0"],["I","#fffff0"],["I","#fffff0"]],
+      [["I","#006400"],["S","#006400"],["S","#006400"],["S","#006400"],["I","#008000"],["S","#008000"],["S","#00008b"],["S","#006400"],["I","#e0ffff"],["I","#5f9ea0"],["I","#9932cc"]],
+      [["S","#faebd7"],["S","#ffff00"],["S","#8b4513"],["R","FeI2","S","#f8f8ff","iron(II) iodide"],["I","#ff8c00"],["S","#ff0000"],["S","#ff0000"],["S","#cd5c5c"],["R","Fe2O3","I","#ff0000","iron(III) oxide"],["S","#ffa500"],["I","#cd853f"]],
+      [["S","#f8f8ff"],["I","#fffff0"],["I","#f0e68c"],["I","#ffff00"],["I","#cd853f"],["I","#fffff0"],["S","#f8f8ff"],["S","#f8f8ff"],["I","#ffff00"],["I","#fffff0"],["I","#cd853f"]],
+      [["S","#f8f8ff"],["S","#ffff00"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#f8f8ff"],["S","#ffff00"],["S","#f8f8ff"],["S","#008000"],["S","#f8f8ff"],["S","#f8f8ff"]]
     ]
 	};
 
@@ -254,62 +254,6 @@ const Beaker = {
 
     return salt;
   }
-
-
-  function dataColourToHex(dataTable)
-{
-    var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
-    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
-    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
-    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
-    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
-    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
-    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
-    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
-    "honeydew":"#f0fff0","hotpink":"#ff69b4",
-    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
-    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
-    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
-    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
-    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
-    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
-    "navajowhite":"#ffdead","navy":"#000080",
-    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
-    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
-    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
-    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
-    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
-    "violet":"#ee82ee",
-    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
-    "yellow":"#ffff00","yellowgreen":"#9acd32"};
-
-    for (let i = 0; i < dataTable.length; i++) // for each row
-    {
-        for (let j = 0; j<dataTable[i].length; j++) // for each column
-        {
-          if (dataTable[i][j][0] == "I" || dataTable[i][j][0] == "S") // if the entry starts with I or S, the colour will be at index 1
-            {
-              if (colours[dataTable[i][j][1].toLowerCase()] != 'undefined'){
-                dataTable[i][j][1] = colours[dataTable[i][j][1].toLowerCase()];
-              }  // convert
-              else{
-                return (dataTable[i][j][1].toLowerCase())
-              }
-            }
-            else { // otherwise it will be at index 3
-              if (colours[dataTable[i][j][3].toLowerCase()] != 'undefined'){
-                dataTable[i][j][3] = colours[dataTable[i][j][3].toLowerCase()];
-              }  // convert
-              else{
-                return (dataTable[i][j][3].toLowerCase())
-              }
-            }
-        }
-    }
-    
-    return dataTable;
-}
-  console.log((dataColourToHex(Constants.reactionTable)));
   let selectedCation = "Ca++";
   let selectedAnion = "Cl-";
 </script>
